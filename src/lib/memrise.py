05815fa93bd86cb54e6dafafcc0b4a443f5d0622
Sam <notsambooker@protonmail.com>
2021-12-02 20:42:30 +0000

    API

--- src/lib/memrise.py
+++ src/lib/memrise.py
@@ -1,6 +1,13 @@
 from requests_futures.sessions import FuturesSession
+from uuid import uuid4
 from bs4 import BeautifulSoup
 from json import loads
+from re import compile
+
+CSRF_PATTERN = compile("csrftoken: \"(.+)\"")
+
+gen_url = lambda route: f"https://app.memrise.com{route}garden/classic_review/" + \
+	"?source_element=course_mode&source_screen=course_details"
 
 class MemriseSession:
 	def __enter__(self):
@@ -40,6 +47,19 @@ class MemriseSession:
 		self.session.headers["x-csrftoken"] = token
 
 
+	def _course_csrf(self, course):
+		route = course['url']
+
+		res = self.session.get(f"https://app.memrise.com{route}garden/classic_review/",
+			headers={
+				"x-csrftoken": None
+			}).result()
+
+		token = CSRF_PATTERN.findall(res.text)[0]
+
+		self.session.headers["x-csrftoken"] = token
+
+
 	def _login(self, username, password):
 		res = self.session.post("https://app.memrise.com/v1.17/auth/access_token/", json={
 			"username": username, "password": password, "grant_type": "password",
@@ -72,3 +92,68 @@ class MemriseSession:
 
 	def close(self):
 		self.session.close()
+
+
+	def course_info(self, course):
+		info = loads(
+			self.session.get(
+				f"https://app.memrise.com/ajax/session/?course_id={course['id']}" + \
+					 f"&course_slug={course['slug']}&session_slug=classic_review"
+			).result().text
+		)
+
+		return info
+
+
+	def solve(self, course, limit=None):
+		self._course_csrf(course)
+
+		info = self.course_info(course)
+
+		route = course["url"]
+
+		course_id = info["session"]["course_id"]
+
+		screens = info["screens"].items()
+
+		points = 200
+		count = len(screens)
+		max_points = count * 200 # Hard limit for max points is 200. Default for points is hard limit
+
+		rem = 0
+		if limit is not None and limit < max_points:
+			points = limit // count
+			rem = limit % count
+
+		for i, (learnable_id, screen) in enumerate(screens):
+			# Question
+			q = list(
+				filter(lambda v: v["template"] == "multiple_choice", \
+					screen.values()
+				)
+			)[0]
+
+			def_element = q["prompt"]["text"]["value"]
+			correct_answer = q["correct"][0]
+			learning_element = def_element
+
+			data = {
+				"box_template": "multiple_choice",
+				"course_id": course_id,
+				"definition_element": def_element,
+				"fully_grow": False,
+				"given_answer": correct_answer,
+				"learnable_id": learnable_id,
+				"learning_element": learning_element,
+				"points": points if i != count - 1 else points + rem,
+				"score": 1,
+				"test_id": str(uuid4()),
+				"time_spent": 2
+			}
+
+			x = self.session.post("https://app.memrise.com/ajax/learning/register",
+				json=data,
+				headers={
+					"referer": gen_url(route)
+				}
+			)
